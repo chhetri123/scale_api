05e15e00e54618db85a111323de55afd97159948
Manish Chhetri <chhetridon123@gmail.com>
2024-11-09 18:48:09 +0545

    added health routes

--- src/app.js
+++ src/app.js
@@ -21,6 +21,9 @@ app.use(limiter);
 
 // Routes
 app.use("/api/users", userRoutes);
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
 
 // Error handling middleware
 app.use((err, req, res, next) => {
